969c99547d19fefe64fe03c56f485e7fecd49202
Nathaniel Fredericks <me@nathfreder.dev>
2019-10-06 12:07:50 -0300

    Add record validations

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,5 +1,9 @@
 class Record < ApplicationRecord
+	validates :name, :dns_type, :content, :cloudflare_id, presence: true
+	validates :name, :cloudflare_id, uniqueness: true
+
   belongs_to :user
+
   attr_readonly :cloudflare_id
   enum dns_type: [ :a, :aaaa, :cname ]
 end
