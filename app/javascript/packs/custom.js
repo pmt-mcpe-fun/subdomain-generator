c06467b49c327f57b4431b1545ac93e4e311396a
Nathaniel Fredericks <me@nathfreder.dev>
2019-10-26 13:48:46 -0300

    Revamp home page

---
+++ app/javascript/packs/custom.js
@@ -0,0 +1,10 @@
+const Typed = require("typed.js")
+
+var typed = new Typed('#sub-name', {
+  strings: ["avengerscity", "minetown", "hsmusical", "pizza", "yeetgames", "myserver", "awesomesauce", "goraptors", "boxofdevsisawesome", "teamtrees", "catgames"],
+  typeSpeed: 100,
+  backSpeed: 50,
+  shuffle: true,
+  loop: true,
+  startDelay: 1000
+});
